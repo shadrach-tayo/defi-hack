5c81f10b7954fcecf12bc088797d6e729ba80545
shadrach <shadrachtemitayo@gmail.com>
2025-08-03 01:28:33 -0600

    ignore lop imports for now

--- foundry/contracts/Twap.sol
+++ foundry/contracts/Twap.sol
@@ -2,12 +2,14 @@
 
 pragma solidity 0.8.23;
 
-import {IPostInteraction} from "./interfaces/IPostInteraction.sol";
+// import {IPostInteraction} from "./interfaces/IPostInteraction.sol";
+import {IPostInteraction} from "@1inch/limit-order-protocol-contract/contracts/interfaces/IPostInteraction.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IOrderMixin} from "./OrderMixin.sol";
+// import {IOrderMixin} from "./OrderMixin.sol";
 import {UniERC20} from "@1inch/solidity-utils/contracts/libraries/UniERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import {IOrderMixin} from "@1inch/limit-order-protocol-contract/contracts/OrderMixin.sol";
+import {IOrderMixin} from "@1inch/limit-order-protocol-contract/contracts/interfaces/IOrderMixin.sol";
 import "hardhat/console.sol";
 
 /// @title TWAP (Time-Weighted Average Price) predicate contract for scheduled execution windows with fill count tracking

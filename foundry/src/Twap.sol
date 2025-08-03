18fde7f3afbc3b982619a11ec06990949e7e09f4
shadrach <shadrachtemitayo@gmail.com>
2025-08-03 08:59:05 -0600

    clean up contract

--- foundry/src/Twap.sol
+++ foundry/src/Twap.sol
@@ -75,50 +75,6 @@ contract TWAP is IPostInteraction, IAmountGetter, Ownable {
 
     receive() external payable {}
 
-    /**
-     * @notice View method that gets called to determine the actual making amount
-     * @param order Order being processed
-     * @param extension Order extension data
-     * @param orderHash Hash of the order being processed
-     * @param taker Taker address
-     * @param takingAmount Actual taking amount
-     * @param remainingMakingAmount Order remaining making amount
-     * @param extraData Extra data
-     */
-    function getMakingAmount(
-        IOrderMixin.Order calldata order,
-        bytes calldata extension,
-        bytes32 orderHash,
-        address taker,
-        uint256 takingAmount,
-        uint256 remainingMakingAmount,
-        bytes calldata extraData
-    ) external view returns (uint256) {
-        return remainingMakingAmount;
-    }
-
-    /**
-     * @notice View method that gets called to determine the actual making amount
-     * @param order Order being processed
-     * @param extension Order extension data
-     * @param orderHash Hash of the order being processed
-     * @param taker Taker address
-     * @param makingAmount Actual taking amount
-     * @param remainingMakingAmount Order remaining making amount
-     * @param extraData Extra data
-     */
-    function getTakingAmount(
-        IOrderMixin.Order calldata order,
-        bytes calldata extension,
-        bytes32 orderHash,
-        address taker,
-        uint256 makingAmount,
-        uint256 remainingMakingAmount,
-        bytes calldata extraData
-    ) external view returns (uint256) {
-        return remainingMakingAmount;
-    }
-
     /// @notice Sets up an execution window for an order
     /// @param orderHash The hash of the order to set up execution window for
     /// @param startTime The start time of the execution window
